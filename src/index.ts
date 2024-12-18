7bd84329ae3faa6d291ab451607961b385024c69
Orwa Watad <theorwa@gmail.com>
2024-12-18 20:50:29 +0200

    'test'

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World! orwa');
+		return new Response('Hello World! orwa1');
 	},
 } satisfies ExportedHandler<Env>;
