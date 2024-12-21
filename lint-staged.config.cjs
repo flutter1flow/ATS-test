296d751c896892bd7e2e7a4f868ee5eab9b54628
Orwa Watad <theorwa@gmail.com>
2024-12-21 17:03:09 +0200

    prepare local test environment added "npm run test-local" command

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-	'**/*.ts': ['eslint --fix', 'prettier --write'],
+	'**/*.ts': ['eslint --fix', 'prettier --write', 'npm run tsc'],
 	'**/*.{json,yml,js,cjs,mjs}': ['prettier --write'],
 };
