980cec78af24ec2fa0dd355717a0bee3dfed8c3c
Orwa Watad <theorwa@gmail.com>
2024-12-25 16:32:21 +0200

    register handlers global

--- src/config/container.config.ts
+++ src/config/container.config.ts
@@ -1,12 +1,10 @@
 import { container } from 'tsyringe';
-import type { TEnv } from '../types/env.type';
 import { HelpHandler } from '@handlers/help.handler';
 import { StartHandler } from '@handlers/start.handler';
 
 export class ContainerConfig {
-	static initialize(env: TEnv): void {
-		console.log('Container start initialization');
-		container.registerInstance('env', env);
+	static registerHandlers(): void {
+		console.log('Registering handlers');
 		container.registerSingleton('ICommandHandler', HelpHandler);
 		container.registerSingleton('ICommandHandler', StartHandler);
 	}
