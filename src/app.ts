0805dacd17c5be92dfa9bca5cb1072a0770f6587
flutter1flow <flutter1flow@gmail.com>
2024-12-25 16:33:12 +0200

    register handlers global (#44)
    Co-authored-by: Orwa Watad <theorwa@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import { container } from 'tsyringe';
 import { CommandRouter } from '@routes/command.router';
 import { formatErrorResponse } from '@utils/error.util';
 
+ContainerConfig.registerHandlers();
+
 export default {
 	async fetch(request: Request, env: TEnv): Promise<Response> {
-		ContainerConfig.initialize(env);
+		container.registerInstance('env', env);
 		return container.resolve(CommandRouter).handleCommand(request).catch(formatErrorResponse);
 	},
 };
