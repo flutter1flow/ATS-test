cf82f23e0e34b2baf38cc98e5767e78f27c62d9c
Orwa Watad <theorwa@gmail.com>
2025-01-01 17:23:49 +0200

    deploy commands

--- scripts/register-webhook.js
+++ scripts/register-webhook.js
@@ -1,7 +1,6 @@
-import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 
-dotenv.config({ path: './.dev.vars' });
+// dotenv.config({ path: './.dev.vars' });
 
 const TELEGRAM_API_BASE = 'https://api.telegram.org/bot';
 
