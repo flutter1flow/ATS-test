3d2ac4d3d48520f7fb89f6b8009206bde5a3eb15
Orwa Watad <theorwa@gmail.com>
2025-01-01 17:17:24 +0200

    deploy commands

---
+++ scripts/deploy-commands.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+const BOT_COMMANDS = [{ command: 'start', description: 'تسجيل' }];
+
+dotenv.config({ path: './.dev.vars' });
+
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+
+if (!BOT_TOKEN) {
+	console.error('Error: BOT_TOKEN is not set in the environment.');
+	process.exit(1);
+}
+
+const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
+
+async function setCommands() {
+	const response = await fetch(`${TELEGRAM_API}/setMyCommands`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ commands: BOT_COMMANDS }),
+	});
+
+	if (response.ok) {
+		console.log('Bot commands updated successfully');
+	} else {
+		const error = await response.json();
+		console.error('Failed to update bot commands:', error);
+	}
+}
+
+setCommands().catch((err) => console.error('Error setting commands:', err));
